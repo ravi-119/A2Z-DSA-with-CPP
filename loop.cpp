97d3c81ccbbca880a785feb70322319b44513d2a
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-01-24 22:51:31 +0530

    loops in cpp committed

---
+++ loop.cpp
@@ -0,0 +1,28 @@
+// while loop
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<< "With the help of while loop" << endl;
+    int a = 10;
+    while( a < 20 )
+    {
+        cout << "value of a: " << a << endl;
+        a++;
+    } 
+    return 0;
+}
+
+// For loop
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << " With the help of for loop" << endl;
+    for( int a = 10; a < 20 ; a = a + 1)
+    {
+        cout<<" Value of a is : " << a << endl;
+    }
+    return 0;
+}
