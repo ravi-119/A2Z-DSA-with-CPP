7bfa90ab237fecb10fbe23126172fb572bf32f8e
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-01-26 14:57:02 +0530

    loop.cpp committed

---
+++ Basics of CPP Programming/loop.cpp
@@ -0,0 +1,45 @@
+// while loop
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<< "With the help of while loop" << endl;
+    int a = 10;
+    while( a < 20 )
+    {
+        cout << "value of a: " << a << endl;
+        a++;
+    } 
+    return 0;
+}
+
+// For loop
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << " With the help of for loop" << endl;
+    for( int a = 10; a < 20 ; a = a + 1)
+    {
+        cout<<" Value of a is : " << a << endl;
+    }
+    return 0;
+}
+
+// Do While loop 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    do
+    {
+        cout<< " Value of a is : " << a << endl;
+        a = a + 1;
+
+    } while(a < 20);
+
+    return 0;
+}
