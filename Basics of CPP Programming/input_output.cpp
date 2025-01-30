5c6fe0e2e1483efb97759e1a83074bade88ba1bb
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-01-30 23:38:44 +0530

    Input and Output committed in cpp

---
+++ Basics of CPP Programming/input_output.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main( )
+{
+ char name [50];
+ cout << "Please enter your name: ";
+ cin >> name;
+ cout << "Your name is: " << name << endl;
+}
+
+
+#include <iostream>
+using namespace std;
+int main( )
+{
+    int age;
+    cout << "Please enter your age: ";
+    cin >> age;
+    cout << "Your age is: " << age << endl;
+}
+
+
+
+
