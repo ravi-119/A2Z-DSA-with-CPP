bbbd96456807b21e64c2b5ad05ceef5c00552ae4
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-02-01 01:25:39 +0530

    decision_making_in_cpp

---
+++ GFG Problems/decision_making_in_cpp.cpp
@@ -0,0 +1,18 @@
+/*
+
+Given two integers, n and m. The task is to check the relation between n and m. Return "lesser" if n < m,  "equal" if n == m, and "greater" if n > m.
+
+*/ 
+
+// Solution 
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        if( n < m )
+        return "lesser";
+        else if(n == m )
+        return "equal";
+        else
+        return "greater";
+    }
+};
