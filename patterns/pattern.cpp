3ef71314c961ee05df35d8eb43fa567608719b1a
ravi-119 <ry277015@gmail.com>
2025-02-20 23:02:01 +0530

    pattern no 11 added

--- patterns/pattern.cpp
+++ patterns/pattern.cpp
@@ -147,19 +147,19 @@ void print10(int n){
 
 }
 
-// void print11(int n){
-//     int start = 1;
-//     for(int i = 0; i<n; i++ ){
-//         if(i%2 == 0) start =1;
-//         else start = 0;
+void print11(int n){
+    int start = 1;
+    for(int i = 0; i<n; i++ ){
+        if(i%2 == 0) start =1;
+        else start = 0;
 
-//         for(int j = 0; j<=i; j++){
-//             cout<<start;
-//             start = 1 - start;
-//         }
-//         cout<<endl;
-//     }
-// }
+        for(int j = 0; j<=i; j++){
+            cout<<start;
+            start = 1 - start;
+        }
+        cout<<endl;
+    }
+}
 
 
 
