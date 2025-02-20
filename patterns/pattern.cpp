a214789b9d825f5fe306ca11bf4a7eb7ed7e4d22
ravi-119 <ry277015@gmail.com>
2025-02-21 01:41:49 +0530

    pattern no 12 added

--- patterns/pattern.cpp
+++ patterns/pattern.cpp
@@ -161,12 +161,32 @@ void print11(int n){
     }
 }
 
+void print12(int n){
+    int spaces = 2*(n-1);
+    for(int i = 1; i<=n; i++){
+        //Numbers
+        for(int j = 1; j<=i; j++){
+            cout<<j;
+        }
+        // Spaces
+        for(int j = 1; j<=spaces; j++){
+            cout<<" ";
+        }
+        //Reverse Number
+        for(int j = i; j>=1; j--){
+            cout<<j;
+        }
+        cout<<endl;
+        spaces-=2;
+    }
+}
+
 
 
 int main() {
     int n;
     cin>>n;
-    print11(n);
+    print12(n);
 }
 
 
