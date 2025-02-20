11d299fbecbf10cbf0ff5c15963c7247e2859274
ravi-119 <ry277015@gmail.com>
2025-02-20 23:00:46 +0530

    pattern no 10 added

--- patterns/pattern.cpp
+++ patterns/pattern.cpp
@@ -133,8 +133,41 @@ void inverted_pyramid(int n){
     }
 }
 
+void print10(int n){
+    for(int i = 1; i<=2*n-1; i++){
+        int star = i;
+
+        if(i>n) star = 2*n-i;
+
+        for(int j = 1; j<=star; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+
+// void print11(int n){
+//     int start = 1;
+//     for(int i = 0; i<n; i++ ){
+//         if(i%2 == 0) start =1;
+//         else start = 0;
+
+//         for(int j = 0; j<=i; j++){
+//             cout<<start;
+//             start = 1 - start;
+//         }
+//         cout<<endl;
+//     }
+// }
 
 
+
+int main() {
+    int n;
+    cin>>n;
+    print11(n);
+}
 
 
 // int main() {
@@ -148,13 +181,3 @@ void inverted_pyramid(int n){
 //     }
 
 // }
-
-
-int main() {
-    int n;
-    cin>>n;
-    erect_pyramid(n);
-    inverted_pyramid(n);
-}
-
-
