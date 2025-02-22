78438d809c3fdc653d5fd066d307e94702775c1b
ravi-119 <ry277015@gmail.com>
2025-02-22 14:31:53 +0530

    pattern no 17 added

--- patterns/pattern.cpp
+++ patterns/pattern.cpp
@@ -222,12 +222,35 @@ void print16(int n){
     }
 }
 
+void print17(int n){
+    for(int i = 0; i<n; i++){
 
+        // Space
+        for(int j = 0; j<n-i-1; j++){
+            cout<<" ";
+        }
+
+        // Alphabet
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for(int j = 1; j<= 2*i+1; j++){
+            cout<<ch;
+            if(j<= breakpoint) ch++;
+            else ch--;
+        }
+
+        // Space
+        for(int j = 0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 int main() {
     int n;
     cin>>n;
-    print16(n);
+    print17(n);
 }
 
 
