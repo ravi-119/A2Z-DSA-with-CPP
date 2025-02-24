7b01b821d0d6d484514108d31ed85fc456f977b8
ravi-119 <ry277015@gmail.com>
2025-02-24 15:05:04 +0530

    pattern no 22 added

--- patterns/pattern.cpp
+++ patterns/pattern.cpp
@@ -333,11 +333,24 @@ void print21(int n){
     }
 }
 
+void print22(int n){
+    for(int i = 0; i<2*n-1; i++){
+        for(int j = 0; j<2*n-1; j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2) - j;
+            int bottom = (2*n-2)- i;
+            cout<<(n - min(min(top, bottom), min(left, right)));
+        }
+        cout<<endl;
+    }
+}
+
 
 int main() {
     int n;
     cin>>n;
-    print21(n);
+    print22(n);
 }
 
 
