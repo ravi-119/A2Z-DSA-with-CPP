3070c743626b127d1be222a723864d238758453d
ravi-119 <ry277015@gmail.com>
2025-08-07 09:31:43 +0530

    Valid Palindrome.cpp

---
+++ LeetCode/LeetCodeBasics/Fibonacci Number.cpp
@@ -0,0 +1,14 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+class Solution {
+    public:
+        int fib(int n) {
+            if(n<=1) return n;
+            int last = fib(n-1);
+            int slast = fib(n-2);
+            return last + slast;
+        }
+};
+
+
+
