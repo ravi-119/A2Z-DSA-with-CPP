44462478e73b94dac6591c4cee7888a9a97cc203
ravi-119 <ry277015@gmail.com>
2025-08-07 09:53:15 +0530

    Print numbers from N to 1.cpp

--- GFG Problems/Recursion/Print 1 To N Without Loop.cpp
+++ GFG Problems/Recursion/Print 1 To N Without Loop.cpp
@@ -3,6 +3,15 @@
 // You are given an integer n. You have  to print all numbers from 1 to n.
 // Note: You must use recursion only, and print all numbers from 1 to n in a single line, separated by spaces.
 
+// Explaination 
+// 🔁 Recursion Behavior Summary:
+// Recursion works like a stack (LIFO - Last In, First Out).
+
+// When you do printNos(n - 1) first, you're pushing calls onto the stack.
+
+// Only after reaching the base case (n == 0), you unwind the stack and print numbers from 1 to n.
+
+
 
 class Solution {
     public:
