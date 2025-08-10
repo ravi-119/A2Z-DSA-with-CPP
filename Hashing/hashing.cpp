0b0b08a3ef230705b4fbaf31502be9475b0c3467
ravi-119 <ry277015@gmail.com>
2025-08-11 00:28:55 +0530

    hashing.cpp is done

---
+++ Hashing/hashing.cpp
@@ -0,0 +1,42 @@
+// Brute Force Approach 
+#include <bits/stdc++.h>
+using namespace std;
+int f(number arr[]){
+    int count = 0;
+    for(int i = 0; i<n; i++){
+        if(arr[i] == number)
+        count++;
+    }
+    return count;
+} 
+
+// Optimized Approach using Hashing 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    // Pre-Computation 
+    int hash[13] = {0};
+    for(int i=0; i<n; i++){
+        hash[arr[i]]+=1;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        // fetching 
+        cout<<hash[number]<<endl;
+    }
+
+    return 0;
+}
+
