2371c289041293298ad8260022ac0d59d4cdab08
ravi-119 <ry277015@gmail.com>
2025-08-13 14:41:15 +0530

    Frequency of the Most Frequent Element.cpp is done

---
+++ LeetCodeProblems/LeetCodeHashing/Frequency of the Most Frequent Element.cpp
@@ -0,0 +1,62 @@
+// 1838. Frequency of the Most Frequent Element
+// The frequency of an element is the number of times it occurs in an array.
+
+// You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment the element at that index by 1.
+
+// Return the maximum possible frequency of an element after performing at most k operations.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,4], k = 5
+// Output: 3
+// Explanation: Increment the first element three times and the second element two times to make nums = [4,4,4].
+// 4 has a frequency of 3.
+// Example 2:
+
+// Input: nums = [1,4,8,13], k = 5
+// Output: 2
+// Explanation: There are multiple optimal solutions:
+// - Increment the first element three times to make nums = [4,4,8,13]. 4 has a frequency of 2.
+// - Increment the second element four times to make nums = [1,8,8,13]. 8 has a frequency of 2.
+// - Increment the third element five times to make nums = [1,4,13,13]. 13 has a frequency of 2.
+// Example 3:
+
+// Input: nums = [3,9,6], k = 2
+// Output: 1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 105
+// 1 <= k <= 105
+
+
+// Solution 
+class Solution {
+    public:
+      // Function to count the frequency of all elements from 1 to N in the array.
+      vector<int> frequencyCount(vvector<int>& nums, int k) {
+          // code here
+          long n = nums.size();
+          sort(nums.begin(), nums.end());
+          long total = 0;
+          long l = 0, r = 0;
+          long res = 0; 
+
+          while(r < n){
+            total += num[r];
+            while(nums[r] * (r-l+1) > (total + k)){
+                total -= nums[l]
+                l++;
+            }
+            res = max(r-l+1);
+            r++;
+
+          }
+          return res;
+
+        }
+};
